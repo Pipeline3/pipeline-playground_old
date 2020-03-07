a7483ea569c6369637f4c457d195ca671d72f014
pipeline-dev <58109270+pipeline-dev@users.noreply.github.com>
2020-03-07 15:04:58 +0900

    [App] - test

--- find-mask/src/App.js
+++ find-mask/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        실시간 마스크 현황
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,7 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
