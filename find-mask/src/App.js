e10f8cdd87b4a305549f31630588408c3685ead3
alleyful <alleyful@naver.com>
2020-03-07 15:10:01 +0900

    [test] code-sandbox
    test-code

--- find-mask/src/App.js
+++ find-mask/src/App.js
@@ -8,17 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         실시간 마스크 현황
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
